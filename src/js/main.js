0c7d132a46138aedb2540a664dd1515848324628
Neylick <neylick@gmail.com>
2023-02-10 16:28:14 +0100

    Simple interactions with buttons

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,19 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+
+function setFocus(map_id)
+{
+	document.getElementById('focus').innerHTML = '<h2> Focus on map ' + map_id + '</h2>';
+}
+
+function toggleAllMapButtons()
+{
+	for(var btn of document.getElementsByClassName('map-btn'))
+	{
+		var is_disabled = btn.classList.contains('disabled');
+		if(is_disabled) btn.classList.remove('disabled');
+		else btn.classList.add('disabled');
+	}
+}
